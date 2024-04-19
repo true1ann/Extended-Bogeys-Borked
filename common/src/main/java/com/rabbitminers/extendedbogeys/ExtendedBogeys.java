b1bc6a1c498e25a90a2437b2ee923bf6da26e092
WeidosOddities <michaeljbug0602@gmail.com>
2024-04-19 02:18:31 -0600

    For menu implementation

--- common/src/main/java/com/rabbitminers/extendedbogeys/ExtendedBogeys.java
+++ common/src/main/java/com/rabbitminers/extendedbogeys/ExtendedBogeys.java
@@ -4,6 +4,8 @@ import com.rabbitminers.extendedbogeys.data.ExtendedBogeysLanguageProvider;
 import com.rabbitminers.extendedbogeys.registry.ExtendedBogeysBlocks;
 import com.rabbitminers.extendedbogeys.registry.ExtendedBogeysBogeySizes;
 import com.rabbitminers.extendedbogeys.registry.ExtendedBogeysBogeyStyles;
+import com.rabbitminers.extendedbogeys.bogeys.compat.Mods;
+import com.rabbitminers.extendedbogeys.bogeys.compat.steamnrails.MenuImpl;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.providers.ProviderType;
 import net.minecraft.data.DataGenerator;
@@ -15,13 +17,14 @@ public class ExtendedBogeys {
     public static final String MOD_ID = "extendedbogeys";
     public static final String MOD_NAME = "Extended Bogeys";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
     public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(ExtendedBogeys.MOD_ID);
 
     public static void init() {
         ExtendedBogeysBlocks.register();
         ExtendedBogeysBogeyStyles.register();
         ExtendedBogeysBogeySizes.register();
+//        Mods.STEAMNRAILS.executeIfInstalled(() -> MenuImpl::register);
+        MenuImpl.register();
     }
 
     public static void gatherData(DataGenerator.PackGenerator gen) {
